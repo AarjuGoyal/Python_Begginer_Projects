fd7015f7cd386113d43497a9f1626356f3236714
AarjuGoyal <goyalaarju@gmail.com>
2016-07-14 20:29:58 +0530

    Addd

--- profanityChecker.py
+++ profanityChecker.py
@@ -1,3 +1,4 @@
+"""A program to read words from a document in the path and then a link can be provided to which the word can be used as input to check its profanity"""
 def read_data():
      lines = open('/Users/aarjugoyal/Documents/python_udacity_beg/SampleData.rtf')
      for word in lines:
